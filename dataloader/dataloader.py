91ef3ef87202b7f19c36102ab4e55a61d4af3d87
Felix <felix.luthman@gmail.com>
2021-10-03 16:57:00 +0200

    add test

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -16,10 +16,12 @@ class Dataloader:
             print('Already loaded')
             return getattr(self, dataset)
         else:
-            #if dataset == 'iemocap':
-                #cls = Iemocap
             mod_name = '.' + dataset
             cls = getattr(importlib.import_module(mod_name, package='dataloader'), dataset.capitalize())
 
             setattr(self, dataset, cls(self.dataset_path))
             return getattr(self, dataset).df
+
+
+    def has_attr(self, attr):
+        return hasattr(self, attr)
