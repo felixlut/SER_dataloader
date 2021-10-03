aba7c30617e17229261b1b1c584cc6f34b3be3e1
Felix <felix.luthman@gmail.com>
2021-10-03 15:18:47 +0200

    try dynamic imports

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,6 +1,6 @@
 import importlib
 
-from .iemocap import Iemocap
+#from .iemocap import Iemocap
 
 class Dataloader:
     
@@ -16,10 +16,10 @@ class Dataloader:
         if hasattr(self, dataset):
             return getattr(self, dataset)
         else:
-            if dataset == 'iemocap':
-                cls = Iemocap
-            #mod_name = '.' + dataset
-            #cls = getattr(importlib.import_module(mod_name), dataset.capitalize())
+            #if dataset == 'iemocap':
+                #cls = Iemocap
+            mod_name = '.' + dataset
+            cls = getattr(importlib.import_module(mod_name), dataset.capitalize())
 
             setattr(self, dataset, cls(self.dataset_path))
             return getattr(self, dataset).df
