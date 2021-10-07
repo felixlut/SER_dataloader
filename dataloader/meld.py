84be6917ee7bcce456d44686b9d80a230b93c85b
Felix <felix.luthman@gmail.com>
2021-10-07 05:31:55 +0200

    Fix meld

--- dataloader/meld.py
+++ dataloader/meld.py
@@ -32,26 +32,29 @@ class Meld:
             f_path = wav_path + f_name
             f_path = os.path.abspath(f_path)
 
-            emo = wav_2_label[f_name[:-4]]
-            emo = self.annotation_mapping[emo]
-            set_str = None
-            if 'train' in f_name:
-                set_str = 'train'
-            if 'test' in f_name:
-                set_str = 'test'
-            if 'dev' in f_name:
-                set_str = 'dev'
+            # Some discrepancies in the original dataset leads to some files not having the correct labeling
+            # Just drop them
+            if f_name[:-4] in wav_2_label:
+                emo = wav_2_label[f_name[:-4]]
+                emo = self.annotation_mapping[emo]
+                set_str = None
+                if 'train' in f_name:
+                    set_str = 'train'
+                if 'test' in f_name:
+                    set_str = 'test'
+                if 'dev' in f_name:
+                    set_str = 'dev'
 
-            data.append({
-                'actor_id'      : wav_2_actor[f_name[:-4]],
-                'lang'          : 'eng',
-                'wav_path'      : f_path,
-                'file_name'     : f_name,
-                'emo'           : emo,
-                'sentiment'     : wav_2_sentiment[f_name[:-4]],
-                'text'          : file_2_transcript[f_name[:-4]],
-                'length'        : wav_2_duration[f_name[:-4]],
-                'set'           : set_str,
-            })
+                data.append({
+                    'actor_id'      : wav_2_actor[f_name[:-4]],
+                    'lang'          : 'eng',
+                    'wav_path'      : f_path,
+                    'file_name'     : f_name,
+                    'emo'           : emo,
+                    'sentiment'     : wav_2_sentiment[f_name[:-4]],
+                    'text'          : file_2_transcript[f_name[:-4]],
+                    'length'        : wav_2_duration[f_name[:-4]],
+                    'set'           : set_str,
+                })
 
         return pd.DataFrame(data)
