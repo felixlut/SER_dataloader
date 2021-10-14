20b2e6748bd9f9b395731ebe9d79fdaf447d86df
Felix <felix.luthman@gmail.com>
2021-10-14 14:59:04 +0200

    add path to phone-like wavs

--- dataloader/emodb.py
+++ dataloader/emodb.py
@@ -23,6 +23,7 @@ class Emodb:
 
     def get_df(self):
         wav_path = self.path + '/wav/wav/'
+        wav_tele_path = self.path + '/wav_telepone/wav_telephone/'
         wav_2_duration = util._get_duration_dict(wav_path, self.path + 'wav_2_duration.csv')
         data = []
         for f_name in tqdm(os.listdir(wav_path)):
@@ -33,12 +34,13 @@ class Emodb:
             f_path = os.path.abspath(f_path)
 
             data.append({
-                'actor_id'  : act_id,
-                'lang'      : 'ger',
-                'wav_path'  : f_path,
-                'file_name' : f_name,
-                'emo'       : emo,
-                'length'    : wav_2_duration[f_name[:-4]]
+                'actor_id'      : act_id,
+                'lang'          : 'ger',
+                'wav_path'      : f_path,
+                'wav_tele_path' : wav_tele_path + f_name,
+                'file_name'     : f_name,
+                'emo'           : emo,
+                'length'        : wav_2_duration[f_name[:-4]]
             })
 
         return pd.DataFrame(data)
