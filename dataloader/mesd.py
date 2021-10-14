ab2b136f2fd7175a69c53ce32af472c817a9b604
Felix <felix.luthman@gmail.com>
2021-10-14 23:34:40 +0200

    add MESD (Mexico)

---
+++ dataloader/mesd.py
@@ -0,0 +1,24 @@
+from dataloader.base_dataset import BaseDataset
+
+
+class Mesd(BaseDataset):
+    def __init__(self, top_path):
+        self.annotation_mapping = {
+            'Anger'     : 'Angry',
+            'Disgust'   : 'Disgusted',
+            'Fear'      : 'Fear',
+            'Happiness' : 'Happy',
+            'Neutral'   : 'Neutral',
+            'Sadness'   : 'Sad',
+        }
+        super().__init__(top_path + 'mesddata/')
+
+    def get_dataset_specific_dict(self, f_name):
+        emo, gender, corpus, text = f_name.split('_')
+        return {
+            'emo'       : self.annotation_mapping[emo],
+            'lang'      : 'spanish (Mexico)',
+            'gender'    : gender,
+            'corpus'    : corpus,
+            'text'      : text
+        }
