4969bd12002cdd051d683e412c982be86049a783
Felix <felix.luthman@gmail.com>
2021-10-14 19:09:39 +0200

    fix path for esd

--- dataloader/esd.py
+++ dataloader/esd.py
@@ -6,6 +6,7 @@ from dataloader.base_dataset import BaseDataset
 
 class Esd(BaseDataset):
     def __init__(self, top_path):
+        self.path = top_path + 'esd-data/'
         self.annotation_mapping = {
             'N': 'Neutral',
             'F': 'Happy',
@@ -17,7 +18,7 @@ class Esd(BaseDataset):
         }
         self.wav_2_label = pd.read_csv(self.path + 'wav_2_label.csv', header=None, index_col=0, squeeze=True).to_dict()
         self.wav_2_set = pd.read_csv(self.path + 'wav_2_set.csv', header=None, index_col=0, squeeze=True).to_dict()
-        super().__init__(top_path + 'emodb-data/')
+        super().__init__(self.path)
 
     def get_dataset_specific_dict(self, f_name):
         act_id = int(f_name)
