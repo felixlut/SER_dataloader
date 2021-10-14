87ef082766844c2c9ca433f0738ea63ac3281156
Felix <felix.luthman@gmail.com>
2021-10-14 16:51:40 +0200

    Add an abstract base_dataset class, and convert emodb to be a child of it

---
+++ dataloader/base_dataset.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+from tqdm import tqdm
+import librosa
+import os
+
+
+class BaseDataset(ABC):
+    def __init__(self, path, annotation_mapping):
+        self.path = path
+        self.wav_path = self.path + '/wav/wav/'
+        self.wav_tele_path = self.path + '/wav_telephone/wav_telephone/'
+        self.annotation_mapping = annotation_mapping
+        self.df = self.get_df()
+
+    @abstractmethod
+    def get_dataset_specific_dict(self, f_name):
+        pass
+    
+    def _load_duration_dict(self):
+        csv_path = self.path + 'wav_2_duration.csv'
+        wav_2_duration = {}
+        if os.path.isfile(csv_path):
+            wav_2_duration = pd.read_csv(csv_path, header=None, index_col=0, squeeze=True).to_dict()
+
+        files = os.listdir(self.wav_path)
+        for f_name in tqdm(files, desc='Load Durations'):
+            if f_name[:-4] not in wav_2_duration.keys():
+                duration = librosa.get_duration(filename=self.wav_path + f_name)
+                wav_2_duration[f_name[:-4]] = duration
+        
+        return wav_2_duration
+
+    def get_df(self):
+        wav_2_duration = self._load_duration_dict()
+        data = []
+        for f_name in tqdm(os.listdir(self.wav_path), desc='Load Dataframe'):
+            f_name = f_name[:-4]
+
+            # Set the dataset specific fields
+            dataset_specific_dict = self.get_dataset_specific_dict(f_name)
+
+            # Set the dataset independent fields
+            f_path = self.wav_path + f_name + '.wav'
+            f_tele_path = self.wav_tele_path + f_name + '.wav'
+            
+            dataset_specific_dict['wav_path']   = f_path
+            dataset_specific_dict['file_name']  = f_name
+            dataset_specific_dict['length']     = wav_2_duration[f_name]
+
+            if os.path.isfile(f_tele_path): dataset_specific_dict['wav_tele_path'] = f_tele_path
+
+            # Make sure that a set of fields are present in the data
+            assert set(['lang', 'wav_path', 'emo', 'length']).issubset(dataset_specific_dict.keys())
+
+            data.append(dataset_specific_dict)
+        
+        return pd.DataFrame(data)
+
--- dataloader/emodb.py
+++ dataloader/emodb.py
@@ -1,15 +1,9 @@
-import pandas as pd
-import os
-from tqdm import tqdm
-
-from . import util
+from base_dataset import BaseDataset
 
 
-class Emodb:
-
+class Emodb(BaseDataset):
     def __init__(self, top_path):
-        self.path = top_path + 'emodb-data'
-        self.annotation_mapping = {
+        annotation_mapping = {
             'N': 'Neutral',
             'F': 'Happy',
             'T': 'Sad',
@@ -18,29 +12,12 @@ class Emodb:
             'E': 'Disgusted',
             'A': 'Fear'
         }
-        self.df = self.get_df()
+        super().__init__(top_path + 'emodb-data', annotation_mapping)
+        self.wav_2_duration = self._load_duration_dict()
 
-
-    def get_df(self):
-        wav_path = self.path + '/wav/wav/'
-        wav_tele_path = self.path + '/wav_telephone/wav_telephone/'
-        wav_2_duration = util._get_duration_dict(wav_path, self.path + 'wav_2_duration.csv')
-        data = []
-        for f_name in tqdm(os.listdir(wav_path)):
-            act_id = f_name[:2]
-            emo = f_name[5]
-            emo = self.annotation_mapping[emo]
-            f_path = wav_path + f_name
-            f_path = os.path.abspath(f_path)
-
-            data.append({
-                'actor_id'      : act_id,
-                'lang'          : 'ger',
-                'wav_path'      : f_path,
-                'wav_tele_path' : wav_tele_path + f_name,
-                'file_name'     : f_name,
-                'emo'           : emo,
-                'length'        : wav_2_duration[f_name[:-4]]
-            })
-
-        return pd.DataFrame(data)
+    def get_dataset_specific_dict(self, f_name):
+        return {
+            'actor_id'  : f_name[:2],
+            'emo'       : self.annotation_mapping[f_name[5]],
+            'lang'      : 'ger',
+        }
