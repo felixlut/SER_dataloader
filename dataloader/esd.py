d84b1d898921e12406e28ab5196dc1462e3d794e
Felix <felix.luthman@gmail.com>
2021-10-14 19:11:58 +0200

    remove esd annotation mapping (not needed cuz csv with labels)

--- dataloader/esd.py
+++ dataloader/esd.py
@@ -7,15 +7,6 @@ from dataloader.base_dataset import BaseDataset
 class Esd(BaseDataset):
     def __init__(self, top_path):
         self.path = top_path + 'esd-data/'
-        self.annotation_mapping = {
-            'N': 'Neutral',
-            'F': 'Happy',
-            'T': 'Sad',
-            'W': 'Angry',
-            'L': 'Bored',
-            'E': 'Disgusted',
-            'A': 'Fear'
-        }
         self.wav_2_label = pd.read_csv(self.path + 'wav_2_label.csv', header=None, index_col=0, squeeze=True).to_dict()
         self.wav_2_set = pd.read_csv(self.path + 'wav_2_set.csv', header=None, index_col=0, squeeze=True).to_dict()
         super().__init__(self.path)
