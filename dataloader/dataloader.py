b30d5891fa901a3ab53f9638ae2a370ee6b7f0aa
Felix <felix.luthman@gmail.com>
2021-10-19 19:48:18 +0200

    meld no longer supported, its actaully awful

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -6,11 +6,14 @@ class Dataloader:
     def __init__(self, dataset_path):
         self.dataset_path = dataset_path
         self.implemented_datasets = ['iemocap', 'emodb', 'ravdess', 'esd', 'cremad', 'meld', 'mesd', 'oreau', 'emovo']
+        self.depricated_datasets = ['meld']
 
 
     def load_dataset(self, dataset, force_reload=False):
         if dataset not in self.implemented_datasets:
             raise NotImplementedError("{} not in {}!".format(dataset, self.implemented_datasets))
+        elif dataset in self.depricated_datasets:
+            raise DeprecationWarning("{} no longer supported!".format(dataset))            
 
         if force_reload or not hasattr(self, dataset):
             mod_name = '.' + dataset
