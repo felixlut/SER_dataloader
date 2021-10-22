e49f1df2cd3334be39f01e6538e854a7ab6d856b
Felix <felix.luthman@gmail.com>
2021-10-23 00:46:52 +0200

    fix bug

--- dataloader/ekorpus.py
+++ dataloader/ekorpus.py
@@ -7,7 +7,6 @@ class Ekorpus(BaseDataset):
     def __init__(self, top_path):
         self.path = top_path + 'ekorpus/'
         self.wav_2_label = pd.read_csv(self.path + 'wav_2_label.csv', index_col=0, squeeze=True).to_dict()
-        self.wav_2_set = pd.read_csv(self.path + 'wav_2_set.csv', index_col=0, squeeze=True).to_dict()
         super().__init__(self.path)
 
     def get_dataset_specific_dict(self, f_name):
