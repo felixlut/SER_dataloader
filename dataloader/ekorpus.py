189633e51c4228d06ca07d30e30597e2be33e6e5
Felix <felix.luthman@gmail.com>
2021-10-24 15:24:17 +0200

    ekorpus stuff

--- dataloader/ekorpus.py
+++ dataloader/ekorpus.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import textgrid
+from tqdm import tqdm
+
 
 from dataloader.base_dataset import BaseDataset
 
@@ -15,6 +18,17 @@ class Ekorpus(BaseDataset):
         }
 
         super().__init__(self.path)
+
+    def get_labels():
+        # Read a TextGrid object from a file.
+        wav_2_label = {}
+        for f_name in tqdm(os.listdir('/kaggle/input/ekorpus'), desc='Load Dataframe'):
+            f_path = '/kaggle/input/ekorpus/' + f_name
+            if f_name[-4:] == 'Grid':
+                tg = textgrid.TextGrid.fromFile(f_path)
+                wav_2_label[f_name[:-9]] = tg[-1][0].mark
+                
+        return wav_2_label
 
     def get_dataset_specific_dict(self, f_name):
         return {
